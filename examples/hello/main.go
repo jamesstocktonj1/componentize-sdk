94cb2e55f557aeb936f2aba975cd07de075e09aa
jamesstocktonj1 <james.stocktonj@gmail.com>
2026-02-16 00:16:30 +0000

    feat: expand example

--- examples/hello/main.go
+++ examples/hello/main.go
@@ -1,18 +1,50 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/jamesstocktonj1/componentize-sdk/net/wasihttp"
 )
 
 func init() {
-	wasihttp.HandleFunc(hello)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/hello", hello)
+	mux.HandleFunc("/echo", echo)
+	mux.HandleFunc("/greet", greeting)
+
+	wasihttp.Handle(mux)
 }
 
 func hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Hello, World!")
+}
+
+func echo(w http.ResponseWriter, r *http.Request) {
+	data, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	_, err = w.Write(data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func greeting(w http.ResponseWriter, r *http.Request) {
+	request := struct {
+		Name string `json:"name"`
+	}{}
+	err := json.NewDecoder(r.Body).Decode(&request)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	fmt.Fprintf(w, "Hello, %s!", request.Name)
 }
 
 func main() {}

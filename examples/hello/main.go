7e6396dba1f19ea60f8a03ce3a96d0f460a8f690
jamesstocktonj1 <james.stocktonj@gmail.com>
2026-03-11 18:57:06 +0000

    feat: add function tracing

--- examples/hello/main.go
+++ examples/hello/main.go
@@ -1,15 +1,27 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 
 	"github.com/jamesstocktonj1/componentize-sdk/net/wasihttp"
+	"github.com/jamesstocktonj1/componentize-sdk/otel/trace"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/codes"
+	tracesdk "go.opentelemetry.io/otel/sdk/trace"
 )
 
+var tracer = otel.Tracer("basic-example")
+
 func init() {
+	otel.SetTracerProvider(tracesdk.NewTracerProvider(
+		tracesdk.WithSpanProcessor(trace.NewSpanProcessor()),
+	))
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/hello", hello)
 	mux.HandleFunc("/echo", echo)
@@ -19,28 +31,55 @@ func init() {
 }
 
 func hello(w http.ResponseWriter, r *http.Request) {
+	ctx, span := tracer.Start(r.Context(), "hello")
+	defer span.End()
+
+	span.SetAttributes(
+		attribute.String("http.method", r.Method),
+		attribute.String("http.host", r.Host),
+	)
+	span.AddEvent("some event")
+
+	otherFunc(ctx)
+
 	fmt.Fprintln(w, "Hello, World!")
 }
 
+func otherFunc(ctx context.Context) {
+	_, span := tracer.Start(ctx, "otherFunc")
+	defer span.End()
+
+	span.SetStatus(codes.Ok, "")
+}
+
 func echo(w http.ResponseWriter, r *http.Request) {
+	_, span := tracer.Start(r.Context(), "echo")
+	defer span.End()
+
 	data, err := io.ReadAll(r.Body)
 	if err != nil {
+		span.RecordError(err)
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
 	_, err = w.Write(data)
 	if err != nil {
+		span.RecordError(err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
 
 func greeting(w http.ResponseWriter, r *http.Request) {
+	_, span := tracer.Start(r.Context(), "greeting")
+	defer span.End()
+
 	request := struct {
 		Name string `json:"name"`
 	}{}
 	err := json.NewDecoder(r.Body).Decode(&request)
 	if err != nil {
+		span.RecordError(err)
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}

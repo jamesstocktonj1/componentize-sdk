2167eeda2ff8a3f8106055624c9e24c0fd0c6570
jamesstocktonj1 <james.stocktonj@gmail.com>
2026-03-15 12:15:29 +0000

    chore: abstract duplicate code

---
+++ internal/httptypes/body.go
@@ -0,0 +1,106 @@
+package httptypes
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"runtime"
+	"sync"
+
+	types "github.com/jamesstocktonj1/componentize-sdk/gen/wasi_http_types"
+	streams "github.com/jamesstocktonj1/componentize-sdk/gen/wasi_io_streams"
+)
+
+// NewIncomingBodyReader wraps a consumed IncomingBody as an io.ReadCloser.
+// The returned http.Header map will be populated with trailers once the body
+// has been fully read or closed.
+func NewIncomingBodyReader(body *types.IncomingBody) (io.ReadCloser, http.Header, error) {
+	streamRes := body.Stream()
+	if streamRes.IsErr() {
+		return nil, nil, fmt.Errorf("failed to open incoming body stream - %+v", streamRes.Err())
+	}
+
+	trailer := http.Header{}
+	return &incomingBody{
+		body:    body,
+		stream:  streamRes.Ok(),
+		trailer: trailer,
+	}, trailer, nil
+}
+
+type incomingBody struct {
+	body   *types.IncomingBody
+	stream *streams.InputStream
+
+	trailer     http.Header
+	trailerOnce sync.Once
+}
+
+var _ io.ReadCloser = (*incomingBody)(nil)
+
+func (r *incomingBody) Read(p []byte) (int, error) {
+	pollable := r.stream.Subscribe()
+	defer pollable.Drop()
+	for !pollable.Ready() {
+		runtime.Gosched()
+	}
+
+	readRes := r.stream.Read(uint64(len(p)))
+	if readRes.IsErr() {
+		if readRes.Err().Tag() == streams.StreamErrorClosed {
+			r.trailerOnce.Do(r.parseTrailers)
+			return 0, io.EOF
+		}
+		return 0, fmt.Errorf("failed to read from input stream - %+v", readRes.Err())
+	}
+
+	data := readRes.Ok()
+	copy(p, data)
+	return len(data), nil
+}
+
+func (r *incomingBody) Close() error {
+	r.trailerOnce.Do(r.parseTrailers)
+
+	if r.stream != nil {
+		r.stream.Drop()
+	}
+
+	if r.body != nil {
+		r.body.Drop()
+		r.body = nil
+	}
+	return nil
+}
+
+func (r *incomingBody) parseTrailers() {
+	futureTrailers := types.IncomingBodyFinish(r.body)
+	defer futureTrailers.Drop()
+	r.body = nil
+
+	trailerRes := futureTrailers.Get()
+	if trailerRes.IsNone() {
+		return
+	}
+
+	outerResult := trailerRes.Some()
+	if outerResult.IsErr() {
+		return
+	}
+
+	innerResult := outerResult.Ok()
+	if innerResult.IsErr() {
+		return
+	}
+
+	optTrailer := innerResult.Ok()
+	if optTrailer.IsNone() {
+		return
+	}
+
+	wasiTrailers := optTrailer.Some()
+	defer wasiTrailers.Drop()
+	for _, kv := range wasiTrailers.Entries() {
+		r.trailer.Add(kv.F0, string(kv.F1))
+	}
+}
--- net/wasihttp/response.go
+++ net/wasihttp/response.go
@@ -4,11 +4,9 @@ import (
 	"errors"
 	"io"
 	"net/http"
-	"runtime"
-	"sync"
 
 	types "github.com/jamesstocktonj1/componentize-sdk/gen/wasi_http_types"
-	streams "github.com/jamesstocktonj1/componentize-sdk/gen/wasi_io_streams"
+	"github.com/jamesstocktonj1/componentize-sdk/internal/httptypes"
 )
 
 func parseFutureResponse(resp *types.FutureIncomingResponse) (*http.Response, error) {
@@ -53,95 +51,5 @@ func newResponseBody(resp *types.IncomingResponse) (io.ReadCloser, http.Header, 
 	if bodyRes.IsErr() {
 		return nil, nil, errors.New("failed to consume incoming response")
 	}
-	body := bodyRes.Ok()
-
-	streamRes := body.Stream()
-	if streamRes.IsErr() {
-		return nil, nil, errors.New("failed to consume incoming response body")
-	}
-	stream := streamRes.Ok()
-
-	trailer := http.Header{}
-	return &responseBody{
-		consumer: resp,
-		body:     body,
-		stream:   stream,
-		trailer:  trailer,
-	}, trailer, nil
-}
-
-type responseBody struct {
-	consumer *types.IncomingResponse
-	body     *types.IncomingBody
-	stream   *streams.InputStream
-
-	trailerLock sync.Mutex
-	trailer     http.Header
-	trailerOnce sync.Once
-}
-
-var _ io.ReadCloser = (*responseBody)(nil)
-
-func (r *responseBody) Close() error {
-	r.trailerOnce.Do(r.parseTrailers)
-
-	if r.stream != nil {
-		r.stream.Drop()
-	}
-
-	if r.body != nil {
-		r.body.Drop()
-		r.body = nil
-	}
-	return nil
-}
-
-func (r *responseBody) Read(p []byte) (n int, err error) {
-	pollable := r.stream.Subscribe()
-	defer pollable.Drop()
-	for !pollable.Ready() {
-		runtime.Gosched()
-		// TODO: handle context herr
-	}
-
-	readRes := r.stream.Read(uint64(len(p)))
-	if readRes.IsErr() {
-		if readRes.Err().Tag() == streams.StreamErrorClosed {
-			r.trailerOnce.Do(r.parseTrailers)
-			return 0, io.EOF
-		}
-		return 0, errors.New(readRes.Err().LastOperationFailed().ToDebugString())
-	}
-	data := readRes.Ok()
-
-	copy(p, data)
-	return len(data), nil
-}
-
-func (r *responseBody) parseTrailers() {
-	r.trailerLock.Lock()
-	defer r.trailerLock.Unlock()
-
-	futureTrailer := types.IncomingBodyFinish(r.body)
-	defer futureTrailer.Drop()
-
-	r.body = nil
-
-	trailerRes := futureTrailer.Get()
-	if trailerRes.IsNone() {
-		return
-	} else if trailerRes.Some().IsErr() {
-		return
-	} else if trailerRes.Some().Ok().IsErr() {
-		return
-	} else if trailerRes.Some().Ok().Ok().IsNone() {
-		return
-	}
-
-	trailer := trailerRes.Some().Ok().Ok().Some()
-	defer trailer.Drop()
-
-	for _, kv := range trailer.Entries() {
-		r.trailer.Add(kv.F0, string(kv.F1))
-	}
+	return httptypes.NewIncomingBodyReader(bodyRes.Ok())
 }

7ffd4e6aba44aa9ea313764050b03f7311a7ea62
jamesstocktonj1 <james.stocktonj@gmail.com>
2026-03-20 22:57:00 +0000

    feat: add basic read/write

--- examples/blobby/main.go
+++ examples/blobby/main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	"fmt"
+	"io"
 	"net/http"
 
+	"github.com/jamesstocktonj1/componentize-sdk/file/blobstore"
 	"github.com/jamesstocktonj1/componentize-sdk/net/wasihttp"
 )
 
@@ -12,7 +13,31 @@ func init() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello, Blobby!")
+	cont, err := blobstore.Create("hello")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer cont.Close()
+
+	obj, err := cont.Open("buffer")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer obj.Close()
+
+	_, err = io.Copy(obj, r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	_, err = io.Copy(w, obj)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
 
 func main() {}

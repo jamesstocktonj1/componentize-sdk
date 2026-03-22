e2e9340a85e2bbadf96d66f825cedc209c1e36ef
jamesstocktonj1 <james.stocktonj@gmail.com>
2026-03-22 17:20:43 +0000

    feat: add basic Dial/Listen implementation

---
+++ net/socket/listener.go
@@ -0,0 +1,48 @@
+package socket
+
+import (
+	"net"
+
+	wasiNetwork "github.com/jamesstocktonj1/componentize-sdk/gen/wasi_sockets_network"
+	wasiTcp "github.com/jamesstocktonj1/componentize-sdk/gen/wasi_sockets_tcp"
+)
+
+// wasiListener implements net.Listener over a WASI TCP socket.
+type wasiListener struct {
+	socket  *wasiTcp.TcpSocket
+	network *wasiNetwork.Network
+	addr    net.Addr
+}
+
+var _ net.Listener = (*wasiListener)(nil)
+
+func (l *wasiListener) Accept() (net.Conn, error) {
+	for {
+		res := l.socket.Accept()
+		if res.IsErr() {
+			code := res.Err()
+			if code == wasiNetwork.ErrorCodeWouldBlock {
+				pollable := l.socket.Subscribe()
+				pollable.Block()
+				pollable.Drop()
+				continue
+			}
+			return nil, wasiErrorToGoError(code)
+		}
+		tuple := res.Ok()
+		return &wasiConn{
+			socket: tuple.F0,
+			reader: tuple.F1,
+			writer: tuple.F2,
+		}, nil
+	}
+}
+
+func (l *wasiListener) Close() error {
+	l.socket.Drop()
+	return nil
+}
+
+func (l *wasiListener) Addr() net.Addr {
+	return l.addr
+}
